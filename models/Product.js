03d8e663d358fe7afe26d07672530e1096ec7faf
Muhammad Wasif <115789986+mwasif666@users.noreply.github.com>
2023-11-08 01:59:53 +0500

    add product

--- models/Product.js
+++ models/Product.js
@@ -1,42 +1,57 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  prodNo: {
-    type: String,
-    required: true,
-    default: 1,
+const productSchema = new Schema(
+  {
+    prodNo: {
+      type: String,
+      required: true,
+      default: 1,
+    },
+    prodTitle: {
+      type: String,
+      required: true,
+    },
+    prodDesc: {
+      type: String,
+      required: true,
+    },
+    prodPrice:{
+        type:String,
+        required:true
+    },
+    prodImg1: {
+      type: String,
+      required: true,
+    },
+    prodImg2: {
+      type: String,
+      required: true,
+    },
+    prodQty: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    prodSize: {
+      type: String,
+      required: true,
+    },
+    prodColor: {
+      type: String,
+      required: true,
+    },
+    prodCategory: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  prodTitle: {
-    type: String,
-    required: true,
-  },
-  prodDesc: {
-    type: String,
-    required: true,
-  },
-  prodImg1: {
-    type: String,
-    required: true,
-  },
-  prodImg2: {
-    type: String,
-    required: true,
-  },
-  prodQty: {
-    type: Number,
-    required: true,
-    default: 1,
-  },
-  prodSize: {
-    type: String,
-    required: true,
-  },
-  prodColor: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model("product", productSchema);
 Product.createIndexes();
--- routes/addProduct.js
+++ routes/addProduct.js
@@ -1,4 +1,134 @@
 const express = require("express");
 const router = express.Router();
+const fetchAdmin = require("../middelware/fetchAdmin");
+const Product = require("../models/Product");
+const { body, validationResult } = require("express-validator");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-router.post("/addproduct");
+// 01 This is our first route for add product
+router.post(
+  "/addproduct",
+  fetchAdmin,
+  upload.single("image"),
+  [
+    body("prodTitle", "Enter a valid product title").isLength({ min: 3 }),
+    body("prodDesc", "Enter a valid product description").isLength({ min: 23 }),
+    body("prodPrice", "Enter a valid product price").isLength({ min: 2 }),
+    body("prodQty", "Enter a valid product qunatity").isLength({ min: 1 }),
+    body("prodSize", "Enter a valid product size").isLength({ min: 1 }),
+    body("prodColor", "Enter a valid product color").isLength({ min: 2 }),
+    body("prodCategory", "Enter a valid product category").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    try {
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+      }
+
+      const maxNo = await Product.find(
+        {},
+        { prodNo: 1 },
+        { sort: { prodNo: -1 } }
+      );
+      const nextProdNo = maxNo ? maxNo.prodNo + 1 : 1;
+      const product = await Product({
+        prodNo: nextProdNo,
+        prodTitle: req.body.prodTitle,
+        prodDesc: req.body.prodDesc,
+        prodPrice: req.body.prodPrice,
+        prodImg1: req.file.path,
+        prodImg2: req.file.path,
+        prodQty: req.body.prodQty,
+        prodSize: req.body.prodSize,
+        prodColor: req.body.prodColor,
+        prodCategory: req.body.prodCategory,
+      });
+      const prod = await product.save();
+      res
+        .status(200)
+        .json({ message: "Product Added Successfully", product: prod });
+    } catch (err) {
+      res.status(401).json({ message: "Some thing went wrong!", error: err });
+    }
+  }
+);
+
+// 02 This is our first route for add product
+router.put(
+  "/updateproduct/:Id",
+  fetchAdmin,
+  upload.single("image"),
+  [
+    body("prodTitle", "Enter a valid product title").isLength({ min: 3 }),
+    body("prodDesc", "Enter a valid product description").isLength({ min: 23 }),
+    body("prodPrice", "Enter a valid product price").isLength({ min: 2 }),
+    body("prodQty", "Enter a valid product qunatity").isLength({ min: 1 }),
+    body("prodSize", "Enter a valid product size").isLength({ min: 1 }),
+    body("prodColor", "Enter a valid product color").isLength({ min: 2 }),
+    body("prodCategory", "Enter a valid product category").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    try {
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+      }
+      const findProd = await Product.findOne({ prodNo: req.body.prodNo });
+      if (!findProd) {
+        res.status(400).json({ errors: errors.array() });
+      }
+      (findProd.prodTitle = req.body.prodTitle),
+        (findProd.prodDesc = req.body.prodDesc),
+        (findProd.prodPrice = req.body.prodPrice),
+        (findProd.prodQty = req.body.prodQty),
+        (findProd.prodColor = req.body.prodColor),
+        (findProd.prodSize = req.body.prodSize),
+        (findProd.prodCategory = req.body.prodCategory);
+      if (req.file) {
+        findProd.prodImg1 = req.file.path;
+      }
+      if (req.file) {
+        findProd.prodImg2 = req.file.path;
+      }
+      const udateProd = await findProd.save();
+      res
+        .status(200)
+        .json({ message: "Product Updated Successfully", product: udateProd });
+    } catch (err) {
+      res.status(401).json({ message: "Some thing went wrong!", error: err });
+    }
+  }
+);
+
+// 03 This router is for delete product
+router.delete("/deleteproduct/:Id", fetchAdmin, async (req, res) => {
+  try {
+    const findProd = await Product.findOne({ prodNo: req.body.prodNo });
+    if (!findProd) {
+      res.status(400).json({ message: "Some thing went wrong!" });
+    }
+    const delProd = await findProd.remove();
+    res
+      .status(200)
+      .json({ message: "Product Deleted Successfully", product: delProd });
+  } catch (err) {
+    res.status(401).json({ message: "Some thing went wrong!", error: err });
+  }
+});
+
+// 04 This api is for view product
+router.get("/getproduct", fetchAdmin, async (req, res) => {
+  try {
+    let find = await Product.find();
+    if (find) {
+      res.status(200).json({ product: find, success: true });
+    } else {
+      res.status(401).json({ success: false });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Some thing went wrong!", error: err });
+  }
+});
+module.exports = router;
